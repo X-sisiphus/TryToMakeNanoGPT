da6696813a81899a05efd98bbb4126634b7357ff
X_sisiphus <1821570960@qq.com>
2026-05-19 15:14:52 +0800

    forward

--- model.py
+++ model.py
@@ -1,11 +1,19 @@
+from turtle import forward
 import torch
 import torch.nn as nn
 
-#embedding
+
 class BigramLanguageModel(nn.Module):
+    #embedding
     def __init__(self, vocabSize):
-        super.__init__
+        super().__init__()
         self.tokenEmbeddingTable = nn.Embedding(
             vocabSize,
             vocabSize
         )
+    #forword
+    #idx是二维张量
+    def forward(self, idx):
+        logits = self.tokenEmbeddingTable(idx)
+        return logits
+    
